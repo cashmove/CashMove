a6b80a5d932db9c66ebf972e1fa2512581dc921a
Pavel Koval <pavel.koval@gismart.com>
2019-02-24 11:24:52 +0300

    added pod 'RAMAnimatedTabBarController'

---
+++ CashMove/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CashMove
+//
+//  Created by Pavel Koval on 2/24/19.
+//  Copyright © 2019 CashMove. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CashMove/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CashMove
+//
+//  Created by Pavel Koval on 2/24/19.
+//  Copyright © 2019 CashMove. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
